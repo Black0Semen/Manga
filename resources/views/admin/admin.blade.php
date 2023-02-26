a07d4fb9fb8eb49a099359459b3a71eb959488bf
Black0Semen <sem.cheornii@yandex.ru>
2023-02-27 01:12:38 +0500

    fix

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts')
 @section('content')
-
-<table>
+<div>
+<table style="border-collapse:collapse;border:3px solid black;">
 	<tr>
-		<td>Id</td>
-		<td>Логин</td>
-		<td>Почта</td>
-		<td>Роль</td>
-		<td>Дата создания профиля</td>
+		<td style="border:1px solid black;">Id</td>
+		<td style="border:1px solid black;">Логин</td>
+		<td style="border:1px solid black;">Почта</td>
+		<td style="border:1px solid black;">Роль</td>
+		<td style="border:1px solid black;">Дата создания профиля</td>
+		<td style="border:1px solid black;">Изменить пользователя</td>
+		<td style="border:1px solid black;">Удалить пользователя</td>
 	</tr>
 	@foreach($users as $user)
 	<tr>
-		<td>{{$user -> id}}</td>
-		<td>{{$user -> namelogin}}</td>
-		<td>{{$user -> email}}</td>
-		<td>{{$user -> roles}}</td>
-		<td>{{$user -> created_at}}</td>
-		<td><form method="post" action="{{route('edit', $user -> id)}}">
+		<td style="border:1px solid black;">{{$user -> id}}</td>
+		<td style="border:1px solid black;">{{$user -> name}}</td>
+		<td style="border:1px solid black;">{{$user -> email}}</td>
+		<td style="border:1px solid black;">{{$user -> roles}}</td>
+		<td style="border:1px solid black;">{{$user -> created_at}}</td>
+		<td style="border:1px solid black;"><form method="post" action="{{route('edit', $user -> id)}}">
 			@csrf
 			<input type="submit" value="Изменить пользователя">
 		</form></td>
-		<td><form method="post" action="{{route('delete', $user -> id)}}">
+		<td style="border:1px solid black;"><form method="post" action="{{route('delete', $user -> id)}}">
 			@csrf
 			@method('DELETE')
 			<input type="submit" value="Удалить пользователя">
@@ -28,4 +30,4 @@
 	</tr>
 	@endforeach
 </table>
-</form>
+</div>
