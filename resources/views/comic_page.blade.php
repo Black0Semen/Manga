dfd3be62f7c79820689e46d8160fd14a908ccbe9
BREUCHT27 <119112204+BREUCHT27@users.noreply.github.com>
2023-04-21 10:59:09 +0500

    комикс

--- resources/views/comic_page.blade.php
+++ resources/views/comic_page.blade.php
@@ -2,10 +2,6 @@
 @section('content')
     <div class="page">
       <div class="page__inner">
-        
-        
-          
-  
   <div class="container container_responsive">
     <div class="media-container">
       <div class="media-sidebar">
@@ -20,7 +16,6 @@
         <div>Добавить в список</div>
       </button>
       </div>
-
   <div class="media-info-list paper">
     <a href="https://mangalib.me/manga-list?types%5B%5D=5" target="_blank" class="media-info-list__item">
       <div class="media-info-list__title">Тип</div>
@@ -50,7 +45,7 @@
       <div class="media-content media-content_side">
         <div id="media_type" data-type="manga" class="media-name section">
           <div class="media-name__body">
-            <div class="media-name__main">Сестра эрцгерцога - самозванка</div>
+            <div class="media-name__main">Сестра эрцгерцогаz</div>
             
                                           <div class="media-name__alt">The Duke’s Imposter Sister</div>
                                     </div>
@@ -75,13 +70,11 @@
             <div class="tabs__wrapper">
               <ul class="tabs__list">
                 <li class="tabs__item tabs__item_active" data-key="info">Информация</li>
-                <li class="tabs__item">Главы</li>
-                <li class="tabs__item">Комментарии</li>
-              <div class="tabs__line" style="display: block; width: 270px; transform: translateX(0px);"></div></ul>
+                <li class="tabs__item tabs__item_active" data-key="chapters">Главы</li>
+              <div class="tabs__line" style="display: block; width: 170px; transform: translateX(0px);"></div></ul>
             </div>
           </div>
-
-          <div class="tabs__content tabs__content_show">
+          <div class="tabs__content">
             <div class="media-section media-section_info">
     <div class="media-description media-description_short">
       <div>
@@ -132,37 +125,124 @@
           </div>
   </div>
 </div>
-          </div>
+          <!-- START:Комментарии -->
+          <h2 class="manga-search__title">
+            <span>Комментарии</span>
+          </h2>
+          <div>
+            <div class="media-section paper">             
+                <section>
+                <div class="comments__form">
+                  <div class="comment-reply">
+                    <div aria-placeholder="Написать комментарий..." class="comment-reply__editor is-dirty" contenteditable="true">
+                      <div>
+                        <br>
+                      </div>
+                    </div> 
+                    <div class="comment-reply__controls">
+                      <div class="comment-reply__right">
+                        <!----> 
+                        <button tabindex="-1" class="comment-reply__send button_md button button_primary">
+                         Отправить
+                        </button>
+                      </div> 
+                    </div>
+                  </div>
+                </div>
+<!----> <!----> 
+<div class="comment">
+  <div class="comment__body">
+    <div class="comment__head">
+      <a href="/user/4059002" class="comment__user">
+        <img src="/uploads/users/4059002/KZMVofNImN9A.gif" alt="4ернобородый" class="comment__avatar"> 
+        <span class="comment__username text-truncate">
+          4ернобородый
+        </span>
+      </a> 
+      <time class="comment__date-time">
+        <time>
+          3 месяца назад
+        </time>
+      </time>
+    </div> 
+    <div class="comment__content">
+      <div>
+        На англе глав на 20 больше
+      </div>
+    </div> 
+    <!----> 
+    <div class="comment__controls">
+      <div class="comment__rating">
+        <button data-action="1" class="button comment__rating-action">
+          <svg class="comment__rating-icon">
+            <use xlink:href="#icon-arrow-up">
 
-          <!-- START:Главы -->
-          <div class="tabs__content">
-                        <div class="media-chapters paper section"><div class="media-chapters-head"><!----> <div class="media-section media-chapters-actions"><div class="button button_sm button_light media-chapters-sort"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 113.144 90.311" class="mr-5"><path d="M54.79 24.23L32.214 1.654l-.003-.003a5.686 5.686 0 00-.41-.371c-.066-.054-.135-.1-.202-.15-.08-.06-.159-.123-.242-.179-.08-.053-.164-.1-.246-.15-.076-.044-.15-.092-.228-.134-.084-.045-.17-.083-.256-.123-.081-.039-.161-.08-.244-.114-.084-.034-.169-.062-.254-.092-.089-.032-.177-.066-.268-.094-.084-.026-.17-.044-.256-.066-.093-.023-.185-.05-.28-.068-.098-.02-.198-.032-.298-.046-.082-.012-.163-.027-.247-.036a5.68 5.68 0 00-1.114 0c-.083.009-.164.024-.247.036-.1.014-.2.026-.299.046-.095.019-.186.045-.28.068-.085.022-.17.04-.255.066-.091.028-.18.062-.268.094-.085.03-.17.058-.254.092-.083.035-.163.075-.244.114-.086.04-.172.078-.256.123-.078.042-.152.09-.228.135-.082.049-.165.096-.246.15-.083.055-.162.117-.242.177-.067.05-.136.097-.202.15a5.63 5.63 0 00-.41.372l-.003.003L1.653 24.23a5.645 5.645 0 007.983 7.983L22.578 19.27v65.395a5.644 5.644 0 1011.288 0V19.271L46.81 32.213a5.645 5.645 0 007.982-7.982z" class="media-chapters-sort__arr-left"></path> <path d="M103.508 58.098L90.566 71.04V5.645a5.644 5.644 0 10-11.289 0V71.04L66.335 58.098a5.645 5.645 0 00-7.982 7.983L80.93 88.658l.004.003c.13.13.267.254.41.371.065.054.134.1.202.151.08.06.158.122.242.178.08.054.163.1.246.15.076.045.15.092.227.134.084.045.17.083.257.123.08.039.16.08.244.114.083.034.168.062.253.092.09.032.177.066.268.094.085.026.17.044.256.066.093.023.185.05.28.068.1.02.2.031.299.046.082.011.163.027.247.035.37.037.743.037 1.114 0 .083-.008.164-.024.247-.035.1-.015.2-.026.299-.046.094-.019.186-.045.279-.069.086-.02.172-.04.256-.065.091-.028.18-.062.268-.094.085-.03.17-.058.253-.093.084-.034.163-.075.244-.113.086-.04.173-.078.257-.124.078-.041.151-.089.227-.134.082-.049.166-.096.246-.15.084-.055.162-.117.242-.177.067-.05.136-.097.202-.151.142-.116.278-.239.408-.368l.006-.006L111.49 66.08a5.645 5.645 0 00-7.982-7.982z" class="media-chapters-sort__arr-right"></path></svg> <span>Сортировать</span></div> <!----> <!----></div></div> <div class="media-section media-chapters-list"><div class="vue-recycle-scroller ready page-mode direction-vertical"><!----> <div class="vue-recycle-scroller__item-wrapper" style="min-height: 480px;"><div class="vue-recycle-scroller__item-view" style="transform: translateY(0px);"><div data-id="2396749" class="media-chapter media-chapter_new" data-is-read="false"><div data-place="right" class="media-chapter__icon media-chapter__icon_read tooltip"><i class="fa"></i></div> <div class="media-chapter__body"><div class="media-chapter__name text-truncate"><a href="https://mangalib.me/gajja-yeodongsaeng-eul-wonhamyeon/v1/c12?ui=5740535" class="link-default">
-        Том 1 Глава 12 <!----></a></div> <div class="media-chapter__username text-truncate"><a href="https://mangalib.me/user/744187" class="link-default"><i class="fa fa-user-o fa-fw"></i>
-        Милистерра
-      </a></div> <!----> <div class="media-chapter__date">
-      04.04.2023
-    </div></div><div class="media-chapter__actions"><a href="https://mangalib.me/manga/gajja-yeodongsaeng-eul-wonhamyeon/chapter/2356340" data-place="left" aria-label="Редактировать главу" class="media-chapter__icon media-chapter__icon_edit tooltip"><i class="fa fa-pencil fa-fw"></i></a> <div data-place="left" aria-label="Скачать главу" class="media-chapter__icon media-chapter__icon_download tooltip"><i class="fa fa-cloud-download fa-fw"></i></div> <!----></div></div></div></div> <!----> <div data-v-b329ee4c="" tabindex="-1" class="resize-observer"><object aria-hidden="true" tabindex="-1" type="text/html" data="./комикс_files/saved_resource.html"></object></div></div> <!----></div></div>
+            </use>
+          </svg>
+        </button> 
+        <div data-vote="3_5" class="comment__rating-value">
+          -2
+        </div> 
+        <button data-action="0" class="button comment__rating-action">
+          <svg class="comment__rating-icon">
+            <use xlink:href="#icon-arrow-down">
+
+            </use>
+          </svg>
+        </button>
+      </div>
+    </div> 
+    <!---->
+  </div> 
+  <!----> <!---->
+</div>
+</div>
+</div> 
+<!---->
+</div>
+</div>
+<!-- START:Главы -->
+<div class="tabs__content">
+  <div class="media-chapters paper section">
+    <div class="media-section media-chapters-list">
+      <div class="vue-recycle-scroller ready page-mode direction-vertical">
+      <div class="vue-recycle-scroller__item-wrapper" style="min-height: 500px;">
+      <div class="vue-recycle-scroller__item-view" style="transform: translateY(0px);">
+      <div class="media-chapter">
+        <div class="media-chapter__body">
+        <div class="media-chapter__name text-truncate">
+            <a href="/the-magic-chef-of-ice-and-fire/v2/c73?ui=5740535" class="link-default">
+        Том 2 Глава 73 - Меч Сюаньцзи
+      </a>
+    </div> 
+    <div class="media-chapter__username">
+      <a href="/user/1063136" class="link-default">
+        Ryslan31
+      </a>
+    </div> <!----> 
+    <div class="media-chapter__date">
+      20.04.2023
+    </div>
+  </div> 
+  <div class="media-chapter__actions">
+    <a href="/manga/the-magic-chef-of-ice-and-fire/chapter/2416092" data-place="left" aria-label="Редактировать главу" class="media-chapter__icon media-chapter__icon_edit tooltip">
+      +
+    </a> 
+  </div>
+</div>
+</div>
+</div> 
+</div>
+</div>
                       </div>
           <!-- END:Главы -->
-          <!-- START:Комментарии -->
-          <div class="tabs__content">
-            <div class="media-section paper">
-                              
-                <section><div class="comments-header">
-                <div class="comments__form"><div class="comments__pseudo-form">
-    Написать комментарий...
-  </div></div> <!----> <!----> <div class="comments-list"><div id="comment_183945110" class="comment"><div class="comment__body"><div class="comment__head"><a href="https://mangalib.me/user/4671250" class="comment__user"><img src="./комикс_files/hV6Z5WpbJe35.jpg" alt="яяяяя чурка" class="comment__avatar"> <span class="comment__username text-truncate">яяяяя чурка</span></a> <time class="comment__date-time"><time datetime="Mon Mar 27 2023 14:21:00 GMT+0500 (Екатеринбург, стандартное время)">8 дней назад</time></time></div> <div class="comment__content"><div>чет, как то не зашло. может когда выйдет больше глав вернусь, но не думаю</div></div> <!----> <div class="comment__controls"><button class="button_link">
-        ответить
-      </button> <div class="dropdown comment__dropdown"><div data-dropdown-trigger="" class="comment__control comment__control_dropdown"><svg><use xlink:href="#icon-extra"></use></svg></div> <!----></div> <div class="comment__rating"><div data-action="1" class="comment__rating-action"><svg class="comment__rating-icon"><use xlink:href="#icon-arrow-up"></use></svg></div> <div data-vote="4_1" class="comment__rating-value comment__rating-value_positive">
-          3
-        </div>
- <div data-action="0" class="comment__rating-action"><svg class="comment__rating-icon"><use xlink:href="#icon-arrow-down"></use></svg></div></div></div> <!----></div> <!----> <!----></div> <!----></div> <div class="comments__load-page"><!----></div></div></section>
+</section>
                 
                           </div>
           </div>
           <!-- END:Комментарии -->
+          </div>
         </div>
-      </div>
     </div>
   </div>
 
@@ -238,6 +318,4 @@
     </div>
   </div>
 </div>
-
-  
-            </div>
+</div>
